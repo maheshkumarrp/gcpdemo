a4728dd34d81e563a7e23f289c76e21b72775738
Mahesh Kumar R P <mahesh-kumar.r-p@hpe.com>
2018-07-26 18:12:11 +0800

    See the magic

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hello World! to Mahesh");
+  response.end("Hello World! Google Next 2018!!!");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
